6ea550f846b077a4b86857e6b7807e0a69eeb4be
Devin Baughman <47373236+gloatoriginal@users.noreply.github.com>
2020-02-03 16:27:39 -0800

    hello

---
+++ scripts/myscript.js
@@ -0,0 +1,10 @@
+document.getElementById("UseThisDiv").innerHTML = "Hello"
+
+document.getElementById("MyButton").onclick = () => {
+	displayFirstNameFunction(document.getElementById("FirstName").value, document.getElementById("LastName").value)
+	anotherFunctionHello(document.getElementById("UseThisDiv"))
+}
+
+function displayFirstNameFunction (fName, lName) {
+	document.getElementById("UseThisDiv").innerHTML = "Hello there: " + fName + " " + lName
+}
