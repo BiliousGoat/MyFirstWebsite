f4fdfb953887a21d4fa37ce89fcb7a2b224d6a9d
Devin Baughman <47373236+gloatoriginal@users.noreply.github.com>
2020-02-04 10:01:34 -0800

    change

--- scripts/myscript.js
+++ scripts/myscript.js
@@ -2,9 +2,13 @@ document.getElementById("UseThisDiv").innerHTML = "Hello"
 
 document.getElementById("MyButton").onclick = () => {
 	displayFirstNameFunction(document.getElementById("FirstName").value, document.getElementById("LastName").value)
-	anotherFunctionHello(document.getElementById("UseThisDiv"))
+	takeBathWaterQuanity(document.getElementById("BathwaterQuantity").value)
 }
 
 function displayFirstNameFunction (fName, lName) {
-	document.getElementById("UseThisDiv").innerHTML = "Hello there: " + fName + " " + lName
+	document.getElementById("UseThisDiv").innerHTML = `Hello there ${fName} ${lName}`
+}
+
+function takeBathWaterQuanity (gallons) {
+	document.getElementById("CustomerTotalDiv").innerHTML = `Hello you have ${gallons} on the way!`
 }
